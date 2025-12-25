67c800c7bc46e2cabe0ec7cf1c6d7c36cdf714e6
sofia <sofiagracia000@gmail.com>
2025-12-25 11:13:50 +0100

    Initial commit

---
+++ src/app/pages/uikit/detaildemo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-demo',
+  standalone: true,
+  imports: [],
+  template: `<p>detaildemo works!</p>`,
+})
+export class DetailDemo {
+
+}
